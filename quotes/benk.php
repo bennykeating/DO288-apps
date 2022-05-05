557253ad641fb7b2ce758c0db4681c50ad7664a7
bennykeating <91073338+bennykeating@users.noreply.github.com>
2022-05-05 12:12:04 -0400

    Update benk.php

--- quotes/benk.php
+++ quotes/benk.php
@@ -6,6 +6,15 @@
 
     $lori = $_ENV["DATABASE_PASSWORD"];
 
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
 
-    //print The password is $lori ! "\n";
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+
+
+echo "Password is " . $lori . "<br>";
+
 ?>
